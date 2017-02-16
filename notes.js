f1cb152a091d7c47d8fdbe4836948ad694ba814e
Brian <riceb53@gmail.com>
2017-02-16 13:45:31 -0500

    add new note and update note

---
+++ notes.js
@@ -0,0 +1,26 @@
+console.log('hi')
+
+var application = new Vue({
+  el: '#application',
+  data: {
+    notes: [
+      {
+        content: "Here is my note",
+        date: new Date()
+      }
+    ],
+    currentNote: 0
+  },
+  methods: {
+    addNote: function() {
+      this.currentNote = this.notes.length
+      this.notes.push({
+        date: new Date(),
+        content: "Write your note here"
+      })
+    },
+    switchNote: function(theIndex) {
+      this.currentNote = theIndex
+    }
+  }
+})
